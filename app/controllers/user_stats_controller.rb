88307e156d9e7f692bba3d7188cc56117c8d0842
mark miranda <notmarkmiranda@gmail.com>
2018-08-05 16:26:34 -0600

    Raise test coverage

--- app/controllers/user_stats_controller.rb
+++ app/controllers/user_stats_controller.rb
@@ -1,13 +1,13 @@
 class UserStatsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
-    if params[:season_id]
-      @season = true
+    if params[:season]
+      @season = Season.find(params[:season])
       @players = @user.players.joins(:game)
-        .where('games.season_id = ?', params[:season_id])
+        .where('games.season_id = ?', params[:season])
         .order('games.date DESC')
     else
-      @season = false
+      @season = nil
       @players = @user.players.joins(:game)
         .order('games.date DESC')
     end
