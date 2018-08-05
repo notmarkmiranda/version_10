b94e1628b6eb249bc195fdf7c1cc77ef79aeca50
mark miranda <notmarkmiranda@gmail.com>
2018-08-05 16:26:34 -0600

    Raise test coverage

--- app/helpers/statistics_partial_helper.rb
+++ app/helpers/statistics_partial_helper.rb
@@ -2,4 +2,8 @@ module StatisticsPartialHelper
   def seasons_show?
     controller_name == 'seasons' && action_name == 'show'
   end
+
+  def decimal_to_percentage(number, precision:)
+    number_to_percentage(number * 100.0, precision: precision)
+  end
 end
