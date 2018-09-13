7c4d68c202520c369ad4b80453a66c7a3f3c3e1a
mark miranda <notmarkmiranda@gmail.com>
2018-09-13 13:50:33 -0600

    Add stats for league on dashboard

--- lib/statistics_compiler.rb
+++ lib/statistics_compiler.rb
@@ -4,6 +4,13 @@ module StatisticsCompiler
     (players_count.to_f / games_count * 100).floor / 100.0
   end
 
+  def average_pot_size
+    total_pot = games.map do |game|
+      (game.buy_in * game.players_count) + game.players.sum(:additional_expense)
+    end.sum
+    (total_pot / games_count * 100).floor / 100
+  end
+
   def no_one_qualifies?
     games_count.zero? || players_count.zero?
   end
