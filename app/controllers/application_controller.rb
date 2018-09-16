67f2d9387ae254de57d71cb35be773161f15edb1
mark miranda <notmarkmiranda@gmail.com>
2018-09-16 11:53:40 -0600

    Reorder rescue from pundit in AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   include Pundit
   helper_method :current_user
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def after_sign_in_path_for(resource)
     session[:user_id] = resource.id
     dashboard_path
   end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   private
 
   def current_user
