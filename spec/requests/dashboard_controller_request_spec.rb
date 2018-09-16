8a81ae6161c86cef4e2347b62823b3088e0488f2
mark miranda <notmarkmiranda@gmail.com>
2018-09-16 16:24:46 -0600

    Add league creation flow

--- spec/requests/dashboard_controller_request_spec.rb
+++ spec/requests/dashboard_controller_request_spec.rb
@@ -2,10 +2,8 @@ require 'rails_helper'
 
 describe 'DashboardController', type: :request do
   context 'GET#show' do
-    let(:user) { create(:user) }
-
     it 'renders the show template' do
-      allow_any_instance_of(ApplicationController).to receive(:current_user) { user }
+      stub_current_user
       get dashboard_path
 
       expect(response.status).to eq(200)
