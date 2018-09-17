72908787039dd685278b2692d24ce93beca4f583
mark miranda <notmarkmiranda@gmail.com>
2018-09-16 20:31:47 -0600

    Add after create action for season and admin

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -71,11 +71,6 @@ RSpec.configure do |config|
     config.default_formatter = "doc"
   end
 
-  # Print the 10 slowest examples and example groups at the
-  # end of the spec run, to help surface which specs are running
-  # particularly slow.
-  config.profile_examples = 10
-
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
@@ -107,6 +102,11 @@ RSpec.configure do |config|
   #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
   #   - http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#zero-monkey-patching-mode
   config.disable_monkey_patching!
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
 
 =end
 end
