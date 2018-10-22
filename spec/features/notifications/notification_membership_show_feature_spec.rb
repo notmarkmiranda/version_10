012d405b215ce0ddeb3375f4b03b94a7123237fc
notmarkmiranda <notmarkmiranda@gmail.com>
2018-10-21 21:13:41 -0600

    closes #46

--- spec/features/notifications/notification_membership_show_feature_spec.rb
+++ spec/features/notifications/notification_membership_show_feature_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 describe 'when a user views a notification' do
-  describe 'the user is an appropriate approving user' do
-    let(:notification) { create(:notification) }
+  let(:notification) { create(:notification) }
+  let(:membership) { notification.notifiable }
 
+  before { membership.update(requestor: notification.actor) }
+
+  describe 'the user is an appropriate approving user' do
     before do
       stub_current_user(notification.recipient)
     end
@@ -20,7 +23,6 @@ describe 'when a user views a notification' do
     context 'and it has a decision'
     let(:admin_membership) { create(:membership, role: 1) }
     let(:other_admin) { admin_membership.user }
-    let(:membership) { notification.notifiable }
 
     context 'approved' do
       before { membership.update(status: 1, decider: other_admin) }
@@ -41,6 +43,20 @@ describe 'when a user views a notification' do
         expect(page).to have_button('Approve', disabled: true)
         expect(page).to have_button('Reject', disabled: true)
       end
+    end
+  end
+
+  describe 'the user is the requestor' do
+    before do
+      stub_current_user(notification.actor)
+    end
+
+    it 'should not allow the user to view buttons on the notification page' do
+      visit notification_path(notification)
+
+      expect(page).to have_content(notification.notification_text)
+      expect(page).to_not have_button('Approve')
+      expect(page).to_not have_button('Reject')
     end
   end
 end
