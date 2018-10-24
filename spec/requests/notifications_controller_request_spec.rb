772cb76a8b526725f6e35bdbf08092a554f98e46
notmarkmiranda <notmarkmiranda@gmail.com>
2018-10-23 19:23:00 -0600

    add request specs for notification controller

--- spec/requests/notifications_controller_request_spec.rb
+++ spec/requests/notifications_controller_request_spec.rb
@@ -15,4 +15,27 @@ describe NotificationsController, type: :request do
       expect(response.status).to eq(302)
     end
   end
+
+  context 'GET#show' do
+    let(:notification) { create(:notification) }
+    it 'renders the show template - user' do
+      stub_current_user(notification.actor)
+
+      get notification_path(notification)
+      expect(response.status).to eq(200)
+    end
+
+    it 'redirects a non-participating user' do
+      stub_current_user
+
+      get notification_path(notification)
+      expect(response.status).to eq(302)
+    end
+
+    it 'redirects to root path - visitor' do
+      get notification_path(notification)
+
+      expect(response.status).to eq(302)
+    end
+  end
 end
