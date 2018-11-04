2ccee94b181529cd706baa994d9670dd6f661dee
notmarkmiranda <notmarkmiranda@gmail.com>
2018-11-04 09:36:00 -0700

    Complete feature spec for game scheduling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,24 @@
 class GamesController < ApplicationController
+  def new
+    @league = League.find(params[:league_id])
+    @game = @league.new_game
+    authorize @game
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to @game
+    end
+  end
+
   def show
     @game = Game.find(params[:id])
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:season_id, :date, :buy_in)
   end
 end
