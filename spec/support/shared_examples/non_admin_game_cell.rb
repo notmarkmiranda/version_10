d425778bc21d49fcf7f2fd37b7e49c3acb60a3d6
notmarkmiranda <notmarkmiranda@gmail.com>
2019-02-04 21:25:30 -0700

    Add button conditional for view

--- spec/support/shared_examples/non_admin_game_cell.rb
+++
@@ -1,9 +0,0 @@
-shared_examples "non-admin game cell" do
-  it 'does not show a score link and complete button' do
-    visit league_path(league)
-
-    expect(page).to have_content(league.name)
-    expect(page).to_not have_button('Complete Game')
-    expect(page).to_not have_link('Score Game')
-  end
-end
