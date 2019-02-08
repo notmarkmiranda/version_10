ba12ce1252834e760df9d4af9c82478e9ec9d7cc
notmarkmiranda <notmarkmiranda@gmail.com>
2019-02-07 18:09:01 -0700

    [61] Add decorator specs

--- spec/decorators/player_decorator_spec.rb
+++ spec/decorators/player_decorator_spec.rb
@@ -1,4 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe PlayerDecorator do
+describe PlayerDecorator, type: :decorator do
+  let(:player) { create(:player).decorate }
+  context '#additional_expense_text' do
+    subject { player.additional_expense_text }
+
+    it 'returns nil' do
+      player.update(additional_expense: 0)
+
+      expect(subject).to be_nil
+    end
+
+    it 'returns the text' do
+      player.update(additional_expense: 1)
+
+      expect(subject).to eq('| Rebuy or Add-on: $1')
+    end
+  end
+
+  context '#score_text' do
+    subject { player.score_text }
+
+    it 'returns the score' do
+      player.update(additional_expense: 0)
+
+      expect(subject).to eq('Score: 1.5 ')
+    end
+
+    it 'returns the score with the additional text' do
+      player.update(additional_expense: 1)
+
+      expect(subject).to eq('Score: 1.5 | Rebuy or Add-on: $1')
+    end
+  end
 end
