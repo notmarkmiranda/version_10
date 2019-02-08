ffd6eeebb0b66754df3924f105f0402ab26b5cd9
notmarkmiranda <notmarkmiranda@gmail.com>
2019-02-07 18:09:01 -0700

    [61] Add decorator specs

--- spec/decorators/notification_decorator_spec.rb
+++ spec/decorators/notification_decorator_spec.rb
@@ -1,4 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe NotificationDecorator do
+describe NotificationDecorator, type: :decorator do
+  let(:notification) { create(:notification).decorate }
+  context '#mark_as_read_link' do
+    subject { notification.mark_as_read_link }
+    it 'returns nil' do
+      notification.update(read_at: Time.now)
+
+      expect(subject).to be_nil
+    end
+
+    it 'returns the link' do
+      notification.update(read_at: nil)
+
+      expected_return = '| <a href="#">mark as read</a>'
+      expect(subject).to eq(expected_return)
+    end
+  end
+
+  context '#notification_status' do
+    subject { notification.notification_status }
+
+    it 'returns the approved half' do
+      notification.notifiable.approve!(User.last)
+
+      expect(subject).to eq('approved less than a minute ago')
+    end
+
+    it 'returns the non-approved half' do
+      notification.notifiable.update(status: 'pending')
+
+      expect(subject).to eq('less than a minute ago')
+    end
+  end
 end
