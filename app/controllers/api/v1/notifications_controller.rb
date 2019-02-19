f1eca63fbe35a6f0cb82a751e58e03463a7d6b3f
notmarkmiranda <notmarkmiranda@gmail.com>
2019-02-19 14:52:15 -0700

    [43] Finish API endpoint

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,9 +1,24 @@
 class Api::V1::NotificationsController < Api::ApiController
   include ActionController::Serialization
   respond_to :json
+  before_action :authenticate_user!
 
   def last_five
-    return render json: :unauthorized, status: 401 unless current_user
     render json: current_user.last_five_notifications, status: 200
+  end
+
+  def mark_as_read
+    if params[:id]
+      notification = current_user.notifications.find(params[:id])
+      notification.mark_as_read!
+    else
+      current_user.notifications.update_all(read_at: Time.now)
+    end
+  end
+
+  private
+
+  def authenticate_user!
+    return render json: :unauthorized, status: 401 unless current_user
   end
 end
